5dbb2956532c286c4a42b227460c315d4d8a53ae
guofengjd <guofengjd@gmail.com>
2015-08-21 17:05:36 +0800

    first time

---
+++ YuCloud/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  YuCloud
+//
+//  Created by 熊国锋 on 15/8/21.
+//  Copyright (c) 2015年 VIROYAL-ELEC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ YuCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YuCloud
+//
+//  Created by 熊国锋 on 15/8/21.
+//  Copyright (c) 2015年 VIROYAL-ELEC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
